3449f9ddeb460d1871c46e807eeeddf07e193bcf
Alan-Ito <alanthanks4yourmail@gmail.com>
2021-11-21 03:05:27 +0900

    投稿作成画面 POST実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
@@ -27,6 +28,13 @@ class PostController extends Controller
     public function create()
     {
         return view('posts/create');
+    }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
