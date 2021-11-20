9302066ed6a94ed20b2fb1df0c7609cb8ad97d0e
Alan-Ito <alanthanks4yourmail@gmail.com>
2021-11-20 14:15:13 +0900

    ブログ投稿一覧画面作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    /**
-     * Post一覧を表示する
-     * 
-     * @param Post Postモデル
-     * @return array Postモデルリスト
-     */
+
     public function index(Post $post)
     {
-        return $post->get();
+        return view("posts/index")->with(["posts" => $post->getPaginateByLimit()]);
     }
 }
+?>
+
